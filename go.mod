dc0cba730a577592d830410a04d962d5f4755b46
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-09-08 16:18:38 +0300

    parsing json into a map

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Parutix/json2go
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Parutix/json2go/parser"
+)
+
+func main() {
+	jsonData := []byte(`{
+  "name": "John Doe",
+  "age": 30,
+  "email": "john.doe@example.com",
+  "address": {
+    "street": "123 Main St",
+    "city": "New York",
+    "zipcode": "10001"
+  },
+  "phoneNumbers": [
+    {
+      "type": "home",
+      "number": "212-555-1234"
+    },
+    {
+      "type": "work",
+      "number": "646-555-4567"
+    }
+  ],
+  "isActive": true,
+  "preferences": {
+    "newsletter": false,
+    "notifications": ["email", "sms"]
+  },
+  "projects": [
+    {
+      "name": "Project A",
+      "budget": 5000.75,
+      "completed": false
+    },
+    {
+      "name": "Project B",
+      "budget": 12000.50,
+      "completed": true
+    }
+  ]
+}`)
+	result, err := parser.ParseJSON(jsonData)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
