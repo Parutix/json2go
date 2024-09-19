1abeff9d7a1c6a21ffe0baff1605848e6b5b92fd
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-09-19 19:05:48 +0300

    modularize cmd

--- cmd/cli.go
+++ cmd/cli.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Parutix/json2go/formatter"
@@ -17,43 +18,55 @@ func checkFileExists(filePath string) bool {
 
 // Run runs the CLI
 func Run() {
-	if len(os.Args) != 4 {
-		fmt.Println("Usage: json2go <input.json> <output.go> <structName>")
+	if err := validateArgs(); err != nil {
+		log.Println(err)
 		return
 	}
 
-	jsonFile := os.Args[1]
-	structName := os.Args[3]
-
-	jsonData, err := os.ReadFile(jsonFile)
+	jsonData, err := readFile(os.Args[1])
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		log.Printf("Error reading file: %v", err)
 		return
 	}
 
 	result, err := parser.ParseJSON(jsonData)
 	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
+		log.Printf("Error parsing JSON: %v", err)
 		return
 	}
 
-	str := generator.GenerateStruct(structName, result)
-	str, err = formatter.FormatStruct(str)
+	str, err := generateAndFormatStruct(os.Args[3], result)
 	if err != nil {
-		fmt.Println("Error formatting struct:", err)
+		log.Printf("Error formatting struct: %v", err)
 		return
 	}
 
-	// Check if the output file exists, if not create it
-	if !checkFileExists(os.Args[2]) {
-		_, err := os.Create(os.Args[2])
-		if err != nil {
-			fmt.Println("Error creating file:", err)
-			return
+	if err := writeFile(os.Args[2], str); err != nil {
+		log.Printf("Error writing file: %v", err)
+	}
+}
+
+func validateArgs() error {
+	if len(os.Args) != 4 {
+		return fmt.Errorf("Usage: json2go <input.json> <output.go> <structName>")
+	}
+	return nil
+}
+
+func readFile(filePath string) ([]byte, error) {
+	return os.ReadFile(filePath)
+}
+
+func writeFile(filePath, content string) error {
+	if !checkFileExists(filePath) {
+		if _, err := os.Create(filePath); err != nil {
+			return fmt.Errorf("Error creating file: %w", err)
 		}
 	}
-	outputFile := os.Args[2]
-	
-	// Write the formatted struct to the output file
-	err = os.WriteFile(outputFile, []byte(str), 0644)
+	return os.WriteFile(filePath, []byte(content), 0644)
+}
+
+func generateAndFormatStruct(structName string, data map[string]interface{}) (string, error) {
+	str := generator.GenerateStruct(structName, data)
+	return formatter.FormatStruct(str)
 }
